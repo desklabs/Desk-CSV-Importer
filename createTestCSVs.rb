22fa98913bff6439a71806b9838eda822f6e9bcd
Eric Dahl <eric@skeyelab.com>
2016-03-23 14:39:47 -0500

    add create CSV script

--- createTestCSVs.rb
+++ createTestCSVs.rb
@@ -6,32 +6,82 @@ Bundler.require(:default)
 
 #binding.pry
 
-comp_ids = []
+rand_limit = 100000000
 
 puts "Creating Company CSV"
+company_ids = []
 CSV.open("CSV_Files/YY_MM_DD_company.csv", "wb") do |csv|
   csv << ["Id","Name"]
   50.times do |i|
-    id = Random.rand(100000000000)
-    comp_ids << id
-    csv << [id, "#{Faker::Company.name}"]
+    id = Random.rand(rand_limit)
+    company_ids << id
+    csv << [id, Faker::Company.name]
   end
 end
 
-puts comp_ids
-#binding.pry
-
 puts "Creating Customer CSV"
+customer_ids = []
 CSV.open("CSV_Files/YY_MM_DD_customer.csv", "wb") do |csv|
-
   csv << ["Id","FirstName","LastName","CompanyId"]
   50.times do |i|
+    id = Random.rand(rand_limit)
+    customer_ids << id
     in_comp = Random.rand(3)
-
     if in_comp <=1
-      csv << [i, "#{Faker::Name.first_name}","#{Faker::Name.last_name}"]
+      csv << [i, Faker::Name.first_name,Faker::Name.last_name]
     else
-      csv << [i, "#{Faker::Name.first_name}","#{Faker::Name.last_name}", comp_ids.sample]
+      csv << [i, Faker::Name.first_name,Faker::Name.last_name, company_ids.sample]
     end
   end
 end
+
+puts "Creating Group CSV"
+group_ids = []
+CSV.open("CSV_Files/YY_MM_DD_group.csv", "wb") do |csv|
+  csv << ["Id","Name"]
+  8.times do |i|
+    id = Random.rand(rand_limit)
+    group_ids << id
+    csv << [id, Faker::Commerce.department(1)]
+  end
+end
+
+puts "Creating User CSV"
+user_ids = []
+CSV.open("CSV_Files/YY_MM_DD_user.csv", "wb") do |csv|
+  csv << ["Id","Name","Email"]
+  20.times do |i|
+    id = Random.rand(rand_limit)
+    user_ids << id
+    csv << [id, Faker::Name.name,Faker::Internet.safe_email]
+  end
+end
+
+puts "Creating Case CSV"
+statuses = ["new","open","pending","resolved"]
+case_ids = []
+CSV.open("CSV_Files/YY_MM_DD_case.csv", "wb") do |csv|
+  csv << ["Id","CustomerId","Subject","Status","CreatedAt","UpdatedAt","UserId","GroupId"]
+  50.times do |i|
+    id = Random.rand(rand_limit)
+    case_ids << id
+
+    status = statuses.sample
+    created_at = Faker::Time.backward(14, :evening).to_datetime.new_offset(0).iso8601
+    row = [id, customer_ids.sample, Faker::Lorem.sentence(3), status, created_at]
+    if status == "new"
+      row << ""
+      row << ""
+      row << ""
+    else
+      row << Faker::Time.between(created_at, DateTime.now).to_datetime.new_offset(0).iso8601
+      row << user_ids.sample
+    end
+
+
+    csv << row
+  end
+end
+
+
+#binding.pry
