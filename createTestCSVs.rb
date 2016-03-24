bec4cfe53a80267b3db7cfa2b8a1f59194f8c8da
Eric Dahl <eric@skeyelab.com>
2016-03-24 09:32:32 -0500

    adding date to cvs names

--- createTestCSVs.rb
+++ createTestCSVs.rb
@@ -6,11 +6,13 @@ Bundler.require(:default)
 
 #binding.pry
 
+todays_date_string = Date.today.year.to_s + "_" + Date.today.month.to_s + "_" + Date.today.day.to_s
+
 rand_limit = 100000000
 
 puts "Creating Company CSV"
 company_ids = []
-CSV.open("CSV_Files/YY_MM_DD_company.csv", "wb") do |csv|
+CSV.open("CSV_Files/#{todays_date_string}_company.csv", "wb") do |csv|
   csv << ["Id","Name"]
   50.times do |i|
     id = Random.rand(rand_limit)
@@ -21,7 +23,7 @@ end
 
 puts "Creating Customer CSV"
 customer_ids = []
-CSV.open("CSV_Files/YY_MM_DD_customer.csv", "wb") do |csv|
+CSV.open("CSV_Files/#{todays_date_string}_customer.csv", "wb") do |csv|
   csv << ["Id","FirstName","LastName","CompanyId"]
   50.times do |i|
     id = Random.rand(rand_limit)
@@ -37,7 +39,7 @@ end
 
 puts "Creating Group CSV"
 group_ids = []
-CSV.open("CSV_Files/YY_MM_DD_group.csv", "wb") do |csv|
+CSV.open("CSV_Files/#{todays_date_string}_group.csv", "wb") do |csv|
   csv << ["Id","Name"]
   8.times do |i|
     id = Random.rand(rand_limit)
@@ -48,7 +50,7 @@ end
 
 puts "Creating User CSV"
 user_ids = []
-CSV.open("CSV_Files/YY_MM_DD_user.csv", "wb") do |csv|
+CSV.open("CSV_Files/#{todays_date_string}_user.csv", "wb") do |csv|
   csv << ["Id","Name","Email"]
   20.times do |i|
     id = Random.rand(rand_limit)
@@ -59,15 +61,17 @@ end
 
 puts "Creating Case CSV"
 statuses = ["new","open","pending","resolved"]
-case_ids = []
-CSV.open("CSV_Files/YY_MM_DD_case.csv", "wb") do |csv|
+cases = []
+
+CSV.open("CSV_Files/#{todays_date_string}_case.csv", "wb") do |csv|
   csv << ["Id","CustomerId","Subject","Status","CreatedAt","UpdatedAt","UserId","GroupId"]
   50.times do |i|
     id = Random.rand(rand_limit)
-    case_ids << id
+    #case_ids << id
 
     status = statuses.sample
     created_at = Faker::Time.backward(14, :evening).to_datetime.new_offset(0).iso8601
+    cases << {:id => id, :created_at => created_at}
     row = [id, customer_ids.sample, Faker::Lorem.sentence(3), status, created_at]
     if status == "new"
       row << ""
@@ -77,11 +81,25 @@ CSV.open("CSV_Files/YY_MM_DD_case.csv", "wb") do |csv|
       row << Faker::Time.between(created_at, DateTime.now).to_datetime.new_offset(0).iso8601
       row << user_ids.sample
     end
-
-
     csv << row
   end
 end
 
+puts "Creating Notes CSV"
+note_ids = []
+CSV.open("CSV_Files/#{todays_date_string}_notes.csv", "wb") do |csv|
+  csv << ["Id","CaseId","UserId","CreatedAt","Body"]
+  200.times do |i|
 
-#binding.pry
+    case_ = cases.sample
+    case_created_at = case_[:created_at]
+    case_id = case_[:id]
+
+    id = Random.rand(rand_limit)
+    csv << [id, case_id,user_ids.sample, Faker::Time.between(case_created_at, DateTime.now).to_datetime.new_offset(0).iso8601, Faker::Hipster.paragraph(2)]
+
+  end
+end
+
+
+binding.pry
