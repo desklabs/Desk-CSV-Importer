11f52c213d31150535509ee968e37f351206c03d
Eric Dahl <eric@skeyelab.com>
2016-04-26 15:33:11 -0500

    almost done

--- company_and_customer_import.rb
+++ company_and_customer_import.rb
@@ -1,18 +1,19 @@
 require 'rubygems'
 require 'bundler/setup'
-#require 'dotenv'
 require 'csv'
-
+require 'logger'
 Bundler.require(:default)
 
+######
+# Pull settings from the .env file.
+######
 Dotenv.load
 
-Rollbar.configure do |config|
-  config.access_token = "c917f3b9237848a88f5cf0dfb306e76c"
-  # Other Configuration Settings
-end
-
-Rollbar.debug("Running Script")
+######
+# Setup our log files
+######
+company_error_log = Logger.new('company_error.log')
+customer_error_log = Logger.new('customer_error.log')
 
 ######
 # Configure the Desk.com connection using the settings in the .env file.
@@ -24,11 +25,9 @@ DeskApi.configure do |config|
   config.endpoint = ENV['DESK_ENDPOINT']
 end
 
-binding.pry
-
 #######
 # We are going to start by importing companies from this file.
-# #####
+#######
 puts "Starting Companies"
 company_csv_file = "./CSV_Files/YY_MM_DD_company.csv"
 
@@ -45,14 +44,19 @@ CSV.foreach(company_csv_file, headers: true) do |row|
   break
   # Store the name and id into our hash from before
   company_hash[row["Id"]] = row["Name"]
+  # create a company for the row
   begin
     new_company = DeskApi.companies.create({
                                              name: row["Name"],
                                              external_id: row["Id"]
 
     })
+    # if it fails, log it in the error log
   rescue DeskApi::Error => e
-    Rollbar.error(e, e.errors, row["Name"])
+    puts "Error creating company: #{row['Name']} - #{e.errors}"
+    company_error_log.error "Error creating company: #{row['Name']} - #{e.errors}"
+  else
+    puts "Success creating company: #{row['Name']}"
   end
 end
 
@@ -60,11 +64,13 @@ end
 #######
 # Now, we will import our customers.  We will be using the hash form above to look
 # up the company name's for our customers.
-# #####
+#######
 puts "Starting Customers"
 company_csv_file = "./CSV_Files/YY_MM_DD_customer.csv"
 
-
+######
+# Loop through the customer CSV file defined above
+######
 CSV.foreach(company_csv_file, headers: true) do |row|
 
   data = {
@@ -81,6 +87,9 @@ CSV.foreach(company_csv_file, headers: true) do |row|
   begin
     new_customer = DeskApi.customers.create(data)
   rescue DeskApi::Error => e
-    Rollbar.error(e, e.errors, emails_array.to_s)
+    puts "Error creating customer: #{row['Name']} - #{e.errors}"
+    customer_error_log.error "Error creating customer: #{row['Name']} - #{e.errors}"
+  else
+    puts "Success creating customer: #{row['Name']}"
   end
 end
